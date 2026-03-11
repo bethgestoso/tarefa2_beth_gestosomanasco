26070e651d5e3c28bf602a7d8157a83de10d84a5
bethgestoso <bethgestoso@gmail.com>
2026-03-11 21:49:51 +0100

    crear enunciado

---
+++ main.py
@@ -0,0 +1,19 @@
+# Tarefa para a UD2.
+# Faker é un paquete que se usa para xerar datos ficticios de usuarios e así poder
+# facer probas nas nosas aplicacións.
+
+# Nesta tarefa debes empregar Faker para desenvolver un programa que cree os datos
+# ficticios de 15 usuarios. Os datos deben almacenarse nun dicionario, de forma que:
+
+# Os usuarios estean identificados por un código comprendido entre o número 1 e o 15.
+# Non poderá haber dous usuarios co mesmo código.
+# Para cada usuario se almacenen os seguintes datos:
+# nome, dirección, correo electrónico, teléfono.
+
+
+# O programa debe mostrar por pantalla os datos de todos os usuarios creados.
+
+
+# Despois, o programa seleccionará aleatoriamente a un dos usuarios para mostrar a
+# seguinte mensaxe: "O usuario chamado NOME foi o afortunado!"
+
